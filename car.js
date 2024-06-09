755546dab6cc551817d383950530dec6fafec309
Ulad L <ulad@Ulads-Air-M2.lan>
2024-06-09 11:52:24 -0500

    3rd

--- car.js
+++ car.js
@@ -1,11 +1,11 @@
-function Factory(make,model,year,color,passengers,convertible,mileage){
-this.make = make;
-this.model = model;
-this.year = year;
-this.color = color;
-this.passengers = passengers;
-this.convertible = convertible;
-this.mileage = mileage;
+function Car(parameter){
+this.make = parameter.make;
+this.model = parameter.model;
+this.year = parameter.year;
+this.color = parameter.color;
+this.passengers = parameter.passengers;
+this.convertible = parameter.convertible;
+this.mileage = parameter.mileage;
 this.started = false;
 
 
@@ -23,19 +23,66 @@ if (this.started){
     alert("Start the engine first.")
 }
 }
-
 }
 
-var chevy = new Factory("Chevy", "Bel Air", 1957, "red", 2, false, 1021);
-var cadi = new Factory("GM", "Cadillac", 1955, "tan", 5, false, 12892);
-var taxi = new Factory("Webville Motors", "Taxi", 1955, "yellow", 4, false, 281341);
-var fiat = new Factory("Fiat", "500", 1957, "Medium Blue", 2, false, 88000);
-var testCar = new Factory("Webville Motors", "Test Car", 2014, "marine", 2, true, 21);
+function Dog(name, breed, weight){
+this.name = name;
+this.breed = breed;
+this.weight = weight;
+this.bark = function () {
+if (this. weight > 25) {
+alert(this.name + " says Woof!");
+} else {
+alert(this.name + " says Yip!");
+}
+}}
 
-var cars = [chevy, cadi, taxi, fiat, testCar];
+var limoParams = {
+    make: "Webville Motors",
+    model: "limo",
+    year: 1983,
+    color: "black",
+    passengers: 12,
+    convertible: false,
+    mileage: 21120
+}
+
+var limo = new Car(limoParams) ;
+var limoDog = new Dog("Rhapsody In Blue", "Poodle", 40);
+
+console.log(limo.make + " " + limo.model + " is a " + typeof limo); console.log (limoDog.name + " is a " + typeof limoDog) ;
+
+if(limo instanceof Car){
+    console.log("Its a car!!")
+}
+/*
+var chevyParams = {
+    make: "Chevy",
+    model: "Bel Air",
+    year: 1957,
+    color: "red",
+    passengers: 2,
+    convertible: false,
+    mileage: 1021
+}
+var chevy = new Factory(chevyParams)
+
+var cadiParams = {
+    make: "GM",
+    model: "Cadillac",
+    year: 1955,
+    color: "tan",
+    passengers: 5,
+    convertible: false,
+    mileage: 12892
+}
+var cadi = new Factory(cadiParams)
+
+
+var cars = [chevy, cadi];
 for(var i = 0; i < cars.length; i++) {
 cars[i].start();
 cars[i].drive();
 cars[i].drive();
 cars[i].stop();
-}
+}*/
