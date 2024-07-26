d1a44d5dd707efd29d90bf1ad5adb3b25eafe309
Ulad L <ulad@Ulads-Air-M2.lan>
2024-07-26 13:44:37 -0500

    hello

--- prototype.js
+++ prototype.js
@@ -1,4 +1,4 @@
-function Robot(name, year, owner) {
+/* function Robot(name, year, owner) {
     this.name = name;
     this.year = year;
     this.owner = owner;
@@ -43,4 +43,38 @@ robby.blinkLights();
 
 console.log(rosie.name + " was made by " + rosie.maker +
 " in "+ rosie.year + " and is owned by " + rosie.owner);
-rosie.cleanHouse();
+rosie.cleanHouse(); */
+
+function Game(){
+    this.level = 0;
+}
+Game.prototype.play = function(){
+    // PLayer stuff
+    this.level++;
+    console.log("Welcome to level " + this.level)
+    this.unlock();
+}
+Game.prototype.unlock = function(){
+
+if(this.level === 42){
+    Robot.prototype.deployLazer = function(){
+        console.log(this.name  + " is blasting you with lazer beams")
+    }
+  }
+}
+
+function Robot(name, year, owner){
+    this.name = name;
+    this.year = year;
+    this.owner = owner;
+}
+
+var game = new Game()
+var robby = new Robot("Robby", 1956, "Dr. Morbius")
+var rosie = new  Robot("Rosie", 1962, "Geroge Jetson")
+
+while(game.level < 42){
+    game.play()
+}
+robby.deployLazer();
+rosie.deployLazer();
